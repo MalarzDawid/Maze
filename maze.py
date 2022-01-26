ce205b8254cc1423b059a54922c26d14261d66fb
MalarzDawid <github@dawidmalarz.pl>
2022-01-26 20:55:15 +0100

    Add Cell class

--- maze.py
+++ maze.py
@@ -2,12 +2,31 @@ import sys
 import pygame
 import random
 
+W = 40
+
+class Cell:
+    def __init__(self, x, y) -> None:
+        self.x = x
+        self.y = y
+
+    def show(self, screen):
+        x = self.x * W
+        y = self.y * W
+        pygame.draw.line(screen, (0, 255, 0), [x, y], [x+W, y])
+        pygame.draw.line(screen, (0, 255, 0), [x+W, y], [x+W, y+W])
+        pygame.draw.line(screen, (0, 255, 0), [x+W, y+W], [x, y+W])
+        pygame.draw.line(screen, (0, 255, 0), [x, y+W], [x, y])
+        
+
 class Maze:
-    def __init__(self, height: int, width: int, block_size: int) -> None:
+    def __init__(self, height: int, width: int, w: int) -> None:
         self.height = height
         self.width = width
-        self.block_size = block_size
-        self.grid_visited = [[False for _ in range(self.width//self.block_size)] for _ in range(self.height//self.block_size)]
+        self.w = w
+        self.rows = self.width//w
+        self.columns = self.height//w
+        self.grid = []
+
         self.screen = pygame.display.set_mode((self.width, self.height))
         self.screen.fill((0, 0, 0))
 
@@ -15,38 +34,19 @@ class Maze:
         pygame.init()
 
     def create(self):
-        for x in range(0, self.width, self.block_size):
-            for y in range(0, self.height, self.block_size):
-                rect = pygame.Rect(x, y, self.block_size, self.block_size)
-                pygame.draw.rect(self.screen, (255, 255, 255), rect, 1)
-        
-        for x in range(1, len(self.grid_visited)-1):
-            for y in range(1, len(self.grid_visited)-1):
-                rect = pygame.Rect(x*self.block_size, y*self.block_size, self.block_size, self.block_size)
-                pygame.draw.rect(self.screen, (0, 255, 0), rect, 0)
-        
-        
-    def dfs(self, x, y):
-        if self.grid_visited[x][y]:
-            return    
-        self.grid_visited[x][y] = True
-        neighbours = [
-            (x-1,y),
-            (x, y + 1),
-            (x+1, y),
-            (x, y-1)
-        ]
-        for neighbour in neighbours:
-            if not self.grid_visited[neighbour[0]][neighbour[1]]:
-                rect = pygame.Rect(neighbour[0], neighbour[1], self.block_size, self.block_size)
-                pygame.draw.rect(self.screen, (0, 255, 0), rect, 0)
-            self.dfs(neighbour[0], neighbour[1])
+        for row in range(self.rows):
+            for column in range(self.columns):
+                cell = Cell(row, column)
+                self.grid.append(cell)
+
+    def draw(self):          
+        for cell in range(len(self.grid)):
+            self.grid[cell].show(self.screen)
 
     def run(self):
+        self.create()
         while True:
-            self.create()
-            # agent = pygame.Rect(y, x, self.block_size, self.block_size)
-            # pygame.draw.rect(self.screen, (0, 255, 0), agent, 0)
+            self.draw()
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     pygame.quit()
